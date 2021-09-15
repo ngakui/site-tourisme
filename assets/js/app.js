9705151007f5c4aa446a1b822a139b132fc1041a
eunji-lee-dev <eunji.lee.dev@gmail.com>
2021-09-15 14:35:30 +0200

    Fix: api

--- assets/js/app.js
+++
@@ -1,4 +0,0 @@
-fetch("/api/records/1.0/search/?dataset=zones-touristiques-internationales&q=&facet=name")
-    .then(response => console.log(response.json()))
-    .then(response => alert(JSON.stringify(response)))
-    .catch(error => alert("Erreur : " + error));
